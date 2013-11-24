7eb81b08474ce254711479bc54193ccbc7a0c235
Sh. Niew <shniew@gmail.com>
2013-11-25 05:10:13 +0800

    arm: real6410: Update board data

--- board/real6410/real6410.c
+++ board/real6410/real6410.c
@@ -29,38 +29,42 @@
 #include <s3c6410.h>
 
 /* ------------------------------------------------------------------------- */
-#define CS8900_Tacs	(0x0)	// 0clk		address set-up
-#define CS8900_Tcos	(0x0)	// 4clk		chip selection set-up
-#define CS8900_Tacc	(0x7)	// 14clk	access cycle
-#define CS8900_Tcoh	(0x0)	// 1clk		chip selection hold
-#define CS8900_Tah	(0x0)	// 4clk		address holding time
-#define CS8900_Tacp	(0x0)	// 6clk		page mode access cycle
-#define CS8900_PMC	(0x0)	// normal(1data)page mode configuration
+#define DM9000_Tacs	(0x0)	/* 0clk		address set-up		*/
+#define DM9000_Tcos	(0x0)	/* 4clk		chip selection set-up	*/
+#define DM9000_Tacc	(0x7)	/* 14clk	access cycle		*/
+#define DM9000_Tcoh	(0x0)	/* 1clk		chip selection hold	*/
+#define DM9000_Tah	(0x0)	/* 4clk		address holding time	*/
+#define DM9000_Tacp	(0x0)	/* 6clk		page mode access cycle	*/
+#define DM9000_PMC	(0x0)	/* normal(1data)page mode configuration	*/
 
 static inline void delay(unsigned long loops)
 {
-	__asm__ volatile ("1:\n" "subs %0, %1, #1\n" "bne 1b":"=r" (loops):"0"(loops));
+	__asm__ volatile ("1:\n" "subs %0, %1, #1\n"
+			  "bne 1b"
+			  : "=r" (loops) : "0" (loops));
 }
 
 /*
  * Miscellaneous platform dependent initialisations
  */
 
-static void cs8900_pre_init(void)
+static void dm9000_pre_init(void)
 {
 	SROM_BW_REG &= ~(0xf << 4);
-	SROM_BW_REG |= (1<<4);
-	SROM_BC1_REG = ((CS8900_Tacs<<28)+(CS8900_Tcos<<24)+(CS8900_Tacc<<16)+(CS8900_Tcoh<<12)+(CS8900_Tah<<8)+(CS8900_Tacp<<4)+(CS8900_PMC));
+	SROM_BW_REG |= ( 1 << 4);
+	SROM_BC1_REG = ((DM9000_Tacs << 28) + (DM9000_Tcos << 24) +
+			(DM9000_Tacc << 16) + (DM9000_Tcoh << 12) +
+			(DM9000_Tah << 8) + (DM9000_Tacp << 4) + DM9000_PMC);
 }
 
 int board_init(void)
 {
 	DECLARE_GLOBAL_DATA_PTR;
 
-	cs8900_pre_init();
+	dm9000_pre_init();
 
 	gd->bd->bi_arch_number = MACH_TYPE;
-	gd->bd->bi_boot_params = (PHYS_SDRAM_1+0x100);
+	gd->bd->bi_boot_params = PHYS_SDRAM_1 + 0x100;
 
 	return 0;
 }
@@ -84,7 +88,7 @@ int checkboard(void)
 #endif
 
 #ifdef CONFIG_ENABLE_MMU
-ulong virt_to_phy_smdk6410(ulong addr)
+ulong virt_to_phy_real6410(ulong addr)
 {
 	if ((0xc0000000 <= addr) && (addr < 0xc8000000))
 		return (addr - 0xc0000000 + 0x50000000);
