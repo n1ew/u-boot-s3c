af287d24600bba3d96bbb34f689cf54860e033cc
Sh. Niew <shniew@gmail.com>
2013-11-26 03:10:18 +0800

    include/common.h: Add S3C64XX SoC support
    The following compile warning before this patch:
    
    interrupts.c: In function ‘interrupt_init’:
    interrupts.c:191:3: warning: implicit declaration of function ‘get_PCLK’

--- include/common.h
+++ include/common.h
@@ -451,7 +451,8 @@ int	prt_mpc8220_clks (void);
 ulong	get_OPB_freq (void);
 ulong	get_PCI_freq (void);
 #endif
-#if defined(CONFIG_S3C2400) || defined(CONFIG_S3C2410) || defined(CONFIG_LH7A40X)
+#if defined(CONFIG_S3C2400) || defined(CONFIG_S3C2410) || \
+	defined(CONFIG_LH7A40X) || defined(CONFIG_S3C64XX)
 ulong	get_FCLK (void);
 ulong	get_HCLK (void);
 ulong	get_PCLK (void);
