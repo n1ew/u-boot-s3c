5d848ba77225baaa24159956128df63cdab10def
Sh. Niew <shniew@gmail.com>
2013-11-26 07:06:19 +0800

    lib_arm/board.c: Remove non-mainline and debug features code

--- lib_arm/board.c
+++ lib_arm/board.c
@@ -44,8 +44,6 @@
 #include <devices.h>
 #include <version.h>
 #include <net.h>
-#include <asm/io.h>
-
 #if defined(CONFIG_BOOT_MOVINAND)
 #include <movi.h>
 #endif
@@ -103,8 +101,8 @@ void mem_malloc_init (ulong dest_addr)
 	mem_malloc_end = dest_addr + CFG_MALLOC_LEN;
 	mem_malloc_brk = mem_malloc_start;
 
-	/* memset ((void *) mem_malloc_start, 0,
-			mem_malloc_end - mem_malloc_start); */
+	memset ((void *) mem_malloc_start, 0,
+			mem_malloc_end - mem_malloc_start);
 }
 
 void *sbrk (ptrdiff_t increment)
@@ -144,11 +142,6 @@ static int display_banner (void)
 	printf ("\n\n%s\n\n", version_string);
 	debug ("U-Boot code: %08lX -> %08lX  BSS: -> %08lX\n",
 	       _armboot_start, _bss_start, _bss_end);
-#ifdef CONFIG_MEMORY_UPPER_CODE /* by scsuh */
-	debug("\t\bMalloc and Stack is above the U-Boot Code.\n");
-#else
-	debug("\t\bMalloc and Stack is below the U-Boot Code.\n");
-#endif
 #ifdef CONFIG_MODEM_SUPPORT
 	debug ("Modem Support enabled\n");
 #endif
@@ -184,8 +177,7 @@ static int display_dram_config (void)
 	for (i=0; i<CONFIG_NR_DRAM_BANKS; i++) {
 		size += gd->bd->bi_dram[i].size;
 	}
-
-	puts("DRAM:    ");
+	puts("DRAM:  ");
 	print_size(size, "\n");
 #endif
 
@@ -195,7 +187,7 @@ static int display_dram_config (void)
 #ifndef CFG_NO_FLASH
 static void display_flash_config (ulong size)
 {
-	puts ("Flash:  ");
+	puts ("Flash: ");
 	print_size (size, "\n");
 }
 #endif /* CFG_NO_FLASH */
@@ -255,28 +247,15 @@ void start_armboot (void)
 #ifndef CFG_NO_FLASH
 	ulong size;
 #endif
-
 #if defined(CONFIG_VFD) || defined(CONFIG_LCD)
 	unsigned long addr;
 #endif
-
 #if defined(CONFIG_BOOT_MOVINAND)
 	uint *magic = (uint *) (PHYS_SDRAM_1);
 #endif
 
 	/* Pointer is writable since we allocated a register for it */
-#ifdef CONFIG_MEMORY_UPPER_CODE /* by scsuh */
-	ulong gd_base;
-
-	gd_base = CFG_UBOOT_BASE + CFG_UBOOT_SIZE - CFG_MALLOC_LEN - CFG_STACK_SIZE - sizeof(gd_t);
-#ifdef CONFIG_USE_IRQ
-	gd_base -= (CONFIG_STACKSIZE_IRQ+CONFIG_STACKSIZE_FIQ);
-#endif
-	gd = (gd_t*)gd_base;
-#else
 	gd = (gd_t*)(_armboot_start - CFG_MALLOC_LEN - sizeof(gd_t));
-#endif
-
 	/* compiler optimization barrier needed for GCC >= 3.4 */
 	__asm__ __volatile__("": : :"memory");
 
@@ -325,11 +304,7 @@ void start_armboot (void)
 #endif /* CONFIG_LCD */
 
 	/* armboot_start is defined in the board-specific linker script */
-#ifdef CONFIG_MEMORY_UPPER_CODE /* by scsuh */
-	mem_malloc_init (CFG_UBOOT_BASE + CFG_UBOOT_SIZE - CFG_MALLOC_LEN - CFG_STACK_SIZE);
-#else
 	mem_malloc_init (_armboot_start - CFG_MALLOC_LEN);
-#endif
 
 #if defined(CONFIG_SMDK6400) || defined(CONFIG_SMDK6410) || defined(CONFIG_SMDK6430) || defined(CONFIG_SMDK2450) || defined(CONFIG_SMDK2416)
 
@@ -358,7 +333,7 @@ void start_armboot (void)
 #else
 
 #if (CONFIG_COMMANDS & CFG_CMD_NAND)
-	puts ("NAND:    ");
+	puts ("NAND:  ");
 	nand_init();		/* go init the NAND */
 #endif
 
@@ -452,7 +427,7 @@ void start_armboot (void)
 #endif
 #if (CONFIG_COMMANDS & CFG_CMD_NET)
 #if defined(CONFIG_NET_MULTI)
-	puts ("Net:     ");
+	puts ("Net:   ");
 #endif
 	eth_initialize(gd->bd);
 #endif
